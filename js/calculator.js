80091b79bba669b2275085e532c97a2eb2e9b938
WRJ7391117 <WRJ7391117@users.noreply.github.com>
2026-05-20 15:50:54 +0000

    首年总发电量+运营期总发电量双卡片

--- js/calculator.js
+++ js/calculator.js
@@ -46,8 +46,8 @@ function calc(p){
     rows.push({yr:y,gen:gen,rev:rev,totCost:totCost,tax:tax,pat:pat,cf:cf,cumCash:cum,vat:vat,sur:sur});
   }
   cfsF[cfsF.length-1]+=deprBase*res;cfsE[cfsE.length-1]+=deprBase*res;
-  var totalRev=0,totalCost=0,totalProfit=0,totalVat=0;for(var i=0;i<rows.length;i++){totalRev+=rows[i].rev;totalCost+=rows[i].totCost;totalProfit+=rows[i].pat;totalVat+=rows[i].vat+rows[i].sur;}
-  return {totalInv:TI,loan:loan,equity:TI-loan,genY1:genY1,totalRev:totalRev,totalCost:totalCost,totalProfit:totalProfit,totalVat:totalVat,irrFull:irr(cfsF),irrEq:irr(cfsE),npvFull:npv(disc,cfsF),payback:payback(cfsF),rows:rows};
+  var totalRev=0,totalCost=0,totalProfit=0,totalVat=0,totalGen=0;for(var i=0;i<rows.length;i++){totalRev+=rows[i].rev;totalCost+=rows[i].totCost;totalProfit+=rows[i].pat;totalVat+=rows[i].vat+rows[i].sur;totalGen+=rows[i].gen;}
+  return {totalInv:TI,loan:loan,equity:TI-loan,genY1:genY1,totalGen:totalGen,totalRev:totalRev,totalCost:totalCost,totalProfit:totalProfit,totalVat:totalVat,irrFull:irr(cfsF),irrEq:irr(cfsE),npvFull:npv(disc,cfsF),payback:payback(cfsF),rows:rows};
 }
 
 function el(id){return document.getElementById(id);}
@@ -106,6 +106,7 @@ function update(){
   setText('resTotalProfit',fm(R.totalProfit));
   setText('resTotalVat',fm(R.totalVat));
   setText('resGenY1',fm(R.genY1));
+  setText('resGenTotal',fm(R.totalGen));
   setText('dispGenY1Total',fm(R.genY1)+' 万度电');
 
   var tb=el('cfTableBody');if(!tb)return;
